9d75813de343e2b6bf908bb03bdffe23fe0b5db8
nathandktran <nathandktran@gmail.com>
2025-03-30 00:20:02 -0500

    part 1

--- Assets/RecordHacks/Scripts/ButtonController.cs
+++ Assets/RecordHacks/Scripts/ButtonController.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+// using System.IO.Ports;
+using System;
 
-public class ButtonController
-{
-    
+public class ButtonController : MonoBehaviour {
+
+  private SpriteRenderer theSR;
+  public Sprite defaultImage;
+  public Sprite pressedImage;
+
+  public KeyCode keyToPress;
+  // SerialPort data_stream = new SerialPort("COM3", 9600);
+  public string receivedstring;
+
+  public KeyCode test = KeyCode.A;
+
+  // Initialization
+  void Start() {
+    theSR = GetComponent<SpriteRenderer>();
+    // data_stream.Open();s
+  }
+
+  // Update once per frame
+  void Update() {
+    if (Input.GetKeyDown(keyToPress)) {
+      theSR.sprite = pressedImage;
+      Debug.Log(keyToPress);
+    }
+
+    if (Input.GetKeyUp(keyToPress)) {
+      
+      theSR.sprite = defaultImage;
+    }
+  }
 }
